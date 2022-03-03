7e8ef161207389111d8a444ec21c1a85e222a39f
IKKIM00 <kimjennfier0429@gmail.com>
2022-03-03 19:28:23 +0900

    update static variable selection part & grn

--- models/grn.py
+++ models/grn.py
@@ -5,10 +5,10 @@ class apply_gating_layer(nn.Module):
     """
     apply Gated Linear Unit to input
     """
-    def __init__(self, hidden_dim, output_dim):
+    def __init__(self, input_dim, hidden_dim):
         super(apply_gating_layer, self).__init__()
-        self.activation_layer = nn.Linear(hidden_dim, output_dim)
-        self.gated_layer = nn.Linear(hidden_dim, output_dim)
+        self.activation_layer = nn.Linear(input_dim, hidden_dim)
+        self.gated_layer = nn.Linear(input_dim, hidden_dim)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
@@ -32,9 +32,9 @@ class gated_residual_network(nn.Module):
             self.dense0 = nn.Linear(input_dim*2, hidden_dim)
         self.dense1 = nn.Linear(hidden_dim, hidden_dim)
         self.elu = nn.ELU()
-        self.glu = apply_gating_layer(hidden_dim, self.output_dim)
+        self.glu = apply_gating_layer(self.hidden_dim, self.output_dim)
         self.dropout = nn.Dropout(p=droupout_rate)
-        self.layer_norm = nn.LayerNorm(hidden_dim)
+        self.layer_norm = nn.LayerNorm(self.output_dim)
 
     def forward(self, x):
         skip = self.output_linear(x)
@@ -45,8 +45,9 @@ class gated_residual_network(nn.Module):
         return self.layer_norm(output)
 
 if __name__ == "__main__":
-    x = torch.rand(128, 1, 23)
-    grn = gated_residual_network(input_dim=23,
-                                 hidden_dim=30)
+    x = torch.rand(32, 2048)
+    grn = gated_residual_network(input_dim=8 * 256,
+                                 hidden_dim=256,
+                                 output_dim=8)
     output = grn(x)
     print(output.shape)
---
+++ models/static.py
@@ -0,0 +1,85 @@
+import json
+
+import torch
+import torch.nn as nn
+from models.grn import gated_residual_network
+
+class static(nn.Module):
+    def __init__(self, model_params):
+        super(static, self).__init__()
+
+        params = dict(model_params)
+
+        self.input_size = int(params['input_size'])
+        self.column_definition = params['column_definition']
+        self.hidden_dim = int(params['hidden_dim'])
+        self.category_counts = json.loads(str(params['category_counts']))
+        self._static_regular_inputs = json.loads(str(params['static_regular_inputs']))
+        self._static_categorical_inputs = json.loads(str(params['static_categorical_inputs']))
+
+        self.num_categorical_variables = len(self.category_counts)
+        self.num_regular_variables = self.input_size - self.num_categorical_variables
+
+        ### Embedding Variables ###
+        embedding_sizes = [
+            self.hidden_dim for i , size in enumerate(self.category_counts)
+        ]
+        self.embeddings = []
+        for i in range(self.num_categorical_variables):
+            embedding = nn.Sequential(
+                nn.Embedding(
+                    num_embeddings=self.category_counts[i],
+                    embedding_dim=embedding_sizes[i])
+            )
+            self.embeddings.append(embedding)
+
+    def forward(self, all_inputs):
+        regular_inputs, categorical_inputs = all_inputs[:, :, :self.num_regular_variables], all_inputs[:, :, self.num_regular_variables:]
+        b, seq_len, c = regular_inputs.size()
+
+        embedded_inputs = [
+            self.embeddings[i](categorical_inputs[Ellipsis, i]) for i in range(self.num_categorical_variables)
+        ]
+        static_inputs = [nn.Linear(1, self.hidden_dim)(regular_inputs[:, 0, i: i + 1]) for i in range(self.num_regular_variables)]\
+                        + [embedded_inputs[i][:, 0, :] for i in range(self.num_categorical_variables)]
+
+        static_inputs = torch.stack(static_inputs, dim=1)
+
+
+class static_combine_and_mask(nn.Module):
+    def __init__(self, model_params):
+        super(static_combine_and_mask, self).__init__()
+
+        params = dict(model_params)
+
+        self.input_size = int(params['input_size'])
+        self.category_counts = json.loads(str(params['category_counts']))
+        self.hidden_dim = int(params['hidden_dim'])
+
+        self.flatten = nn.Flatten()
+        self.grn = gated_residual_network(input_dim=self.input_size * self.hidden_dim,
+                                          hidden_dim=self.hidden_size,
+                                          output_dim=self.input_size,
+                                          droupout_rate=self.dropout)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, embedding):
+        """
+        :param embedding: (batch_size, seq, c)
+        :return:
+        """
+        flatten = self.flatten(embedding)
+        mlp_output = self.grn(flatten)
+        sparse_weights = self.softmax(mlp_output)
+        sparse_weights = torch.unsqueeze(sparse_weights, dim=-1)
+
+        trans_emb_list = []
+        for i in range(self.num_statics):
+            e = gated_residual_network(input_dim=self.hidden_dim,
+                                       hidden_dim=self.hidden_dim)(embedding[:, i:i + 1, :])
+            trans_emb_list.append(e)
+        transformed_embedding = torch.cat(trans_emb_list, dim=1)
+        combined = torch.mul(sparse_weights, transformed_embedding)
+
+        static_vec = torch.sum(combined, dim=1)
+        return static_vec, sparse_weights
