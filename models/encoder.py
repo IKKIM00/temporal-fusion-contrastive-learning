0b53ae30ba073951847f40b672d8b7f1702e5265
IKKIM00 <kimjennifer0429@gmail.com>
2022-05-12 16:14:17 +0900

    update main & encoder with baseline model update

--- models/encoder.py
+++ models/encoder.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn as nn
-from models.grn import gated_residual_network
+from models.autoregressive import PositionalEncoding
 
-class cnn_encoder(nn.Module):
+
+class BaseEncoder(nn.Module):
     def __init__(self, model_params, static_use=False):
-        super(cnn_encoder, self).__init__()
+        super(BaseEncoder, self).__init__()
 
         params = dict(model_params)
         self.kernel_size = int(params['kernel_size'])
@@ -23,8 +24,7 @@ class cnn_encoder(nn.Module):
         self.conv_block1 = nn.Sequential(
             nn.Conv1d(self.input_channels, 32,
                       kernel_size=self.kernel_size,
-                      stride=self.stride,
-                      bias=False),
+                      stride=self.stride),
             nn.BatchNorm1d(32),
             nn.ReLU(),
             nn.MaxPool1d(kernel_size=16, stride=4),
@@ -34,8 +34,7 @@ class cnn_encoder(nn.Module):
         self.conv_block2 = nn.Sequential(
             nn.Conv1d(32, 64,
                       kernel_size=self.kernel_size,
-                      stride=1,
-                      bias=False),
+                      stride=1),
             nn.BatchNorm1d(64),
             nn.ReLU(),
             nn.MaxPool1d(kernel_size=16, stride=4)
@@ -44,8 +43,7 @@ class cnn_encoder(nn.Module):
         self.conv_block3 = nn.Sequential(
             nn.Conv1d(64, self.output_dim,
                       kernel_size=self.kernel_size,
-                      stride=1,
-                      bias=False),
+                      stride=1),
             nn.BatchNorm1d(self.output_dim),
             nn.ReLU(),
             nn.MaxPool1d(kernel_size=16, stride=4),
@@ -69,233 +67,113 @@ class cnn_encoder(nn.Module):
         return logits, x
 
 
-class cnn_encoder_2(nn.Module):
-    def __init__(self, model_params, static_use=False):
-        super(cnn_encoder_2, self).__init__()
+class SimclrHAREncoder(nn.Module):
+    def __init__(self, model_params):
+        super(SimclrHAREncoder, self).__init__()
 
         params = dict(model_params)
         self.kernel_size = int(params['kernel_size'])
         self.input_channels = int(params['input_size'])
         self.stride = int(params['stride'])
-        self.dropout = float(params['dropout'])
-        if static_use:
-            self.feature_len = int(params['static_feature_len'])
-        else:
-            self.feature_len = int(params['feature_len'])
-        self.output_dim = int(params['encoder_output_dim'])
         self.num_classes = int(params['num_classes'])
-        self.static_use = static_use
 
-        self.flatten = nn.Flatten()
         self.conv_block1 = nn.Sequential(
             nn.Conv1d(self.input_channels, 32,
                       kernel_size=self.kernel_size,
-                      stride=self.stride,
-                      bias=False),
+                      stride=self.stride),
             nn.ReLU(),
-            nn.Dropout(self.dropout)
+            nn.Dropout(p=0.1)
         )
 
         self.conv_block2 = nn.Sequential(
             nn.Conv1d(32, 64,
                       kernel_size=self.kernel_size,
-                      stride=1,
-                      bias=False),
+                      stride=1),
             nn.ReLU(),
-            nn.Dropout(self.dropout)
+            nn.Dropout(p=0.1)
         )
 
         self.conv_block3 = nn.Sequential(
-            nn.Conv1d(64, self.output_dim,
+            nn.Conv1d(64, 96,
                       kernel_size=self.kernel_size,
-                      stride=1,
-                      bias=False),
+                      stride=1),
             nn.ReLU(),
-            nn.Dropout(self.dropout)
+            nn.Dropout(p=0.1)
         )
 
         self.globalMaxPool1D = nn.AdaptiveMaxPool1d(1)
+        self.logits = nn.Linear(96, self.num_classes)
 
-
-        self.static_linear = nn.Linear(self.output_dim, 32)
-
-        model_output_dim = self.feature_len
-
-        self.logits = nn.Linear(model_output_dim * self.output_dim, self.num_classes)
-
-        # attach head
-    def forward(self, obs_input, static_input=None):
+    def forward(self, obs_input):
         x = self.conv_block1(obs_input)
-        if self.static_use:
-            static_input = self.static_linear(static_input).unsqueeze(-1)
-            x = torch.cat([x, static_input], dim=2)
         x = self.conv_block2(x)
         x = self.conv_block3(x)
 
         # global Max Pooling 1D
         x = self.globalMaxPool1D(x).squeeze()
+        return self.logits(x), x
 
-        logits = self.logits(x)
-        return logits, x
 
-class cnn_encoder_3(nn.Module):
-    def __init__(self, model_params, static_use=False):
-        super(cnn_encoder_3, self).__init__()
+class CSSHAREncoder(nn.Module):
+    def __init__(self, model_params):
+        super(CSSHAREncoder, self).__init__()
 
         params = dict(model_params)
         self.kernel_size = int(params['kernel_size'])
-
-        
         self.input_channels = int(params['input_size'])
+        self.input_seq = int(params['input_seq'])
         self.stride = int(params['stride'])
         self.dropout = float(params['dropout'])
-
-        if static_use:
-            self.feature_len = int(params['static_feature_len'])
-        else:
-            self.feature_len = int(params['feature_len'])
         self.output_dim = int(params['encoder_output_dim'])
         self.num_classes = int(params['num_classes'])
-        self.static_use = static_use
 
-        self.flatten = nn.Flatten()
         self.conv_block1 = nn.Sequential(
-            nn.Conv1d(self.input_channels, 32,
+            nn.Conv1d(self.input_channels, 64,
                       kernel_size=self.kernel_size,
                       stride=self.stride,
-                      bias=False),
-            nn.BatchNorm1d(32),
-            nn.MaxPool1d(kernel_size=16, stride=4),
-            nn.ReLU()
-        )
-
-        self.conv_block2 = nn.Sequential(
-            nn.Conv1d(32, 64,
-                      kernel_size=self.kernel_size,
-                      stride=1,
-                      bias=False),
+                      padding='same'),
             nn.BatchNorm1d(64),
-            nn.MaxPool1d(kernel_size=16, stride=4),
             nn.ReLU()
         )
-
-        self.conv_block3 = nn.Sequential(
-            nn.Conv1d(64, self.output_dim,
+        self.conv_block2 = nn.Sequential(
+            nn.Conv1d(64, 128,
                       kernel_size=self.kernel_size,
                       stride=1,
-                      bias=False),
-            nn.BatchNorm1d(self.output_dim),
-            nn.MaxPool1d(kernel_size=16, stride=4),
+                      padding='same'),
+            nn.BatchNorm1d(128),
+            nn.ReLU()
+        )
+        self.conv_block3 = nn.Sequential(
+            nn.Conv1d(128, 256,
+                      kernel_size=self.kernel_size,
+                      stride=1,
+                      padding='same'),
+            nn.BatchNorm1d(256),
             nn.ReLU()
         )
 
-        # self.globalMaxPool1D = nn.AdaptiveMaxPool1d(1)
+        self.positional_encodig = PositionalEncoding(256)
+        encoder_layer = nn.TransformerEncoderLayer(d_model=256, nhead=8, batch_first=True)
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=6)
 
-
-        self.static_linear = nn.Linear(self.output_dim, 32)
-
-        model_output_dim = self.feature_len * self.output_dim
-
+        self.flatten = nn.Flatten()
         self.logits = nn.Sequential(
-            nn.Linear(model_output_dim, model_output_dim//2),
-            nn.Dropout(),
+            nn.Linear(self.input_seq * 256, 256),
             nn.ReLU(),
-            nn.Linear(model_output_dim//2, model_output_dim//4),
-            nn.Dropout(),
+            nn.Dropout(p=0.2),
+            nn.Linear(256, 128),
             nn.ReLU(),
-            nn.Linear(model_output_dim//4, self.num_classes),
-            nn.Dropout(),
-            nn.ReLU()
+            nn.Dropout(p=0.2),
+            nn.Linear(128, self.num_classes)
         )
-        
 
-        # attach head
-    def forward(self, obs_input, static_input=None):
-        
+    def forward(self, obs_input):
         x = self.conv_block1(obs_input)
-
-        # if self.static_use:
-        #     static_input = self.static_linear(static_input).unsqueeze(-1)
-        #     x = torch.cat([x, static_input], dim=2)
-
         x = self.conv_block2(x)
         x = self.conv_block3(x)
-        
-        logit = self.flatten(x)
-        
-        logits = self.logits(logit)
-        return logits, x
 
-
-
-class lstm_encoder(nn.Module):
-    def __init__(self, model_params, static_info=False):
-        super(lstm_encoder, self).__init__()
-
-        params = dict(model_params)
-
-        self.input_dim = int(params['input_size'])
-        self.output_dim = int(params['encoder_output_dim'])
-        self.feature_len = int(params['feature_len'])
-        self.dropout = float(params['dropout'])
-        self.num_classes = int(params['num_classes'])
-        self.static_info = static_info
-
-        self.lstm = nn.LSTM(input_size=self.input_dim,
-                            hidden_size=self.output_dim,
-                            batch_first=True)
-
-        if static_info:
-            self.static_context_state_h = gated_residual_network(input_dim=self.output_dim,
-                                                                 hidden_dim=self.output_dim,
-                                                                 droupout_rate=self.dropout)
-            self.static_context_state_c = gated_residual_network(input_dim=self.output_dim,
-                                                                 hidden_dim=self.output_dim,
-                                                                 droupout_rate=self.dropout)
-            self.adaptive_pooling = nn.AdaptiveAvgPool1d(self.feature_len + 1)
-            self.logits = nn.Linear(self.output_dim * 2, self.num_classes)
-        else:
-            self.adaptive_pooling = nn.AdaptiveAvgPool1d(self.feature_len)
-            self.logits = nn.Linear(self.output_dim, self.num_classes)
-
-    def forward(self, x, static_vec=None, static_enrichment_vec=None):
-        if self.static_info:
-            static_h = self.static_context_state_h(static_vec.unsqueeze(0))
-            static_c = self.static_context_state_c(static_vec.unsqueeze(0))
-            output, (h_t, c_t) = self.lstm(x, (static_h, static_c))
-            h_t = torch.cat([h_t.squeeze(), static_enrichment_vec], dim=1)
-
-        else:
-            output, (h_t, c_t) = self.lstm(x)
-        output = self.adaptive_pooling(torch.permute(output, (0, 2, 1)).contiguous())
-        logits = self.logits(h_t)
-        return logits.squeeze(), output
-
-
-if __name__ == '__main__':
-    from data_formatters.dlr import DLRFomatter
-    from libs.dataloader import data_generator
-    from models.static import *
-
-    dataformatter = DLRFomatter()
-    dataset_dir = '../datasets/dlr_preprocessed/'
-    X_train, y_train, X_valid, y_valid, X_test, y_test = dataformatter.split_data(dataset_dir=dataset_dir)
-    model_params, aug_params, loss_params = dataformatter.get_experiment_params()
-
-    train_loader, _, _ = data_generator(X_train, y_train, X_valid, y_valid, X_test, y_test,
-                                        aug_params, 'mobiact', 'CNN', 'self_supervised')
-    dataiter = iter(train_loader)
-    observed_real, y, aug1, aug2, static = dataiter.next()
-
-    model = cnn_encoder(model_params, static_use=True)
-    # model = lstm_encoder(model_params, static_info=False)
-    # static_embedding_model = StaticEmbedding(model_params, 'cpu')
-    # static_variable_selection_model = StaticVariableSelection(model_params, 'cpu')
-    # static_embedding = static_embedding_model(static)
-    # static_context_enrichment, static_vec, sparse_weights = static_variable_selection_model(static_embedding)
-    # logits, output = model(observed_real.float(), static_vec)
-    # logits, output = model(observed_real.float(), static_vec, static_context_enrichment)
-    logits, output = model(observed_real.float(), static.float())
-    print(logits.shape, output.shape, y.shape)
-    # LSTM - (1, 512, 20), (512, 32, 30)
-    # CNN - (512, 20), (512, 32, 30)
+        x = self.positional_encodig(x)
+        x = self.transformer_encoder(x)
+        x = self.flatten(x)
+        logit = self.logits(x)
+        return logit, x
