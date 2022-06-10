a9c36f88e4209da53fe44c337cc9cbecd2bbfea1
Juwan-s <dlacksdn352@gmail.com>
2022-06-10 13:14:52 +0900

    shell fix2

--- running_shell.sh
+++ running_shell.sh
@@ -28,7 +28,7 @@ do
             mkdir -p log/$1/$data/$mode/static_use/${aug_list[${i}]}_and_${aug_list[${j}]}
             python main.py --model_type $1 --experiment_description static_use --static_use --no-sampler_use\
              --dataset $data --device $3 --training_mode $mode --loss_func 'focal'\
-             --aug_method1 $aug1 --aug_method2 $aug2 | tee log/$1/$data/$mode/static_use/${aug_list[${i}]}_and_${aug_list[${j}]}/log.txt
+             --aug_method1 ${aug_list[${i}]} --aug_method2 ${aug_list[${j}]} | tee log/$1/$data/$mode/static_use/${aug_list[${i}]}_and_${aug_list[${j}]}/log.txt
           else
             mkdir -p log/$1/$data/$mode/no_static/${aug_list[${i}]}_and_${aug_list[${j}]}
             python main.py --model_type $1 --experiment_description no-static_use --no-sampler_use\
