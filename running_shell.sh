eacde4c0a64b8b8873bfd90c8601d84520537075
Juwan-s <dlacksdn352@gmail.com>
2022-06-10 13:03:36 +0900

    echo del

--- running_shell.sh
+++ running_shell.sh
@@ -25,27 +25,27 @@ do
         for ((j=i; j<10; j++))
         do
           if [ $2 == 'yes' ]; then
-            echo "mkdir -p log/$1/$data/$mode/static_use/${aug_list[${i}]}_and_${aug_list[${j}]}"
-            echo "python main.py --model_type $1 --experiment_description static_use --static_use --no-sampler_use\
+            mkdir -p log/$1/$data/$mode/static_use/${aug_list[${i}]}_and_${aug_list[${j}]}
+            python main.py --model_type $1 --experiment_description static_use --static_use --no-sampler_use\
              --dataset $data --device $3 --training_mode $mode --loss_func 'focal'\
-             --aug_method1 $aug1 --aug_method2 $aug2 | tee log/$1/$data/$mode/static_use/${aug_list[${i}]}_and_${aug_list[${j}]}/log.txt"
+             --aug_method1 $aug1 --aug_method2 $aug2 | tee log/$1/$data/$mode/static_use/${aug_list[${i}]}_and_${aug_list[${j}]}/log.txt
           else
-            echo "mkdir -p log/$1/$data/$mode/no_static/${aug_list[${i}]}_and_${aug_list[${j}]}"
-            echo "python main.py --model_type $1 --experiment_description no-static_use --no-sampler_use\
+            mkdir -p log/$1/$data/$mode/no_static/${aug_list[${i}]}_and_${aug_list[${j}]}
+            python main.py --model_type $1 --experiment_description no-static_use --no-sampler_use\
              --dataset $data --device $3 --training_mode $mode --loss_func 'focal'\
-             --aug_method1 $aug1 --aug_method2 $aug2 | tee log/$1/$data/$mode/no_static/${aug_list[${i}]}_and_${aug_list[${j}]}/log.txt"
+             --aug_method1 $aug1 --aug_method2 $aug2 | tee log/$1/$data/$mode/no_static/${aug_list[${i}]}_and_${aug_list[${j}]}/log.txt
           fi
         done
       done
     else
       if [ $2 == 'yes' ]; then
-        echo "mkdir -p log/$1/$data/$mode/static_use/"
-        echo "python main.py --model_type $1 --experiment_description static_use --static_use --no-sampler_use\
-       --dataset $data --device $3 --training_mode $mode --loss_func 'focal' | tee log/$1/$data/$mode/static_use/log.txt"
+        mkdir -p log/$1/$data/$mode/static_use/
+        python main.py --model_type $1 --experiment_description static_use --static_use --no-sampler_use\
+       --dataset $data --device $3 --training_mode $mode --loss_func 'focal' | tee log/$1/$data/$mode/static_use/log.txt
       else
-        echo "mkdir -p log/$1/$data/$mode/no_static/"
-        echo "python main.py --model_type $1 --experiment_description no-static_use --no-sampler_use\
-       --dataset $data --device $3 --training_mode $mode --loss_func 'focal' | tee log/$1/$data/$mode/no_static/log.txt"
+        mkdir -p log/$1/$data/$mode/no_static/
+        python main.py --model_type $1 --experiment_description no-static_use --no-sampler_use\
+       --dataset $data --device $3 --training_mode $mode --loss_func 'focal' | tee log/$1/$data/$mode/no_static/log.txt
      fi
     fi
   done
