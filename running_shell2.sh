16b2f47691da4f8a8ca839a1b067d73c04dfaeed
Juwan-s <dlacksdn352@gmail.com>
2022-06-16 14:49:26 +0900

    shell fix

--- running_shell2.sh
+++ running_shell2.sh
@@ -19,13 +19,25 @@ do
   for mode in $training_mode
   do
     if [ $mode == 'self_supervised' ]; then
-      mkdir -p log/$1/$data/$mode/no_static/
-      python main.py --model_type $1 --experiment_description no-static_use --no-sampler_use\
-     --dataset $data --device $3 --training_mode $mode --loss_func 'focal' | tee log/$1/$data/$mode/no_static/log.txt
+      if [ $data == 'mobiact' ]; then
+        mkdir -p log/$1/$data/$mode/no_static/
+        python main.py --model_type $1 --experiment_description no-static_use --no-sampler_use\
+       --dataset $data --device $3 --training_mode $mode --loss_func 'focal' | tee log/$1/$data/$mode/no_static/log.txt
+      else
+        mkdir -p log/$1/$data/$mode/no_static/
+        python main.py --model_type $1 --experiment_description no-static_use --no-sampler_use\
+       --dataset $data --device $3 --training_mode $mode --loss_func 'focal' --batch_size 128 | tee log/$1/$data/$mode/no_static/log.txt
+      fi
     else
-      mkdir -p log/$1/$data/$mode/no_static/
-      python main.py --model_type $1 --experiment_description no-static_use --no-sampler_use\
-     --dataset $data --device $3 --training_mode $mode --loss_func 'focal' | tee log/$1/$data/$mode/no_static/log.txt
+      if [ $data == 'mobiact' ]; then
+        mkdir -p log/$1/$data/$mode/no_static/
+        python main.py --model_type $1 --experiment_description no-static_use --no-sampler_use\
+       --dataset $data --device $3 --training_mode $mode --loss_func 'focal' | tee log/$1/$data/$mode/no_static/log.txt
+      else
+        mkdir -p log/$1/$data/$mode/no_static/
+        python main.py --model_type $1 --experiment_description no-static_use --no-sampler_use\
+       --dataset $data --device $3 --training_mode $mode --loss_func 'focal' --batch_size 128 | tee log/$1/$data/$mode/no_static/log.txt
+      fi
     fi
   done
 done
